085f225ff86a5f3560cecafac6a85f2dfc446aef
EvansRobbie <evansrobbie5311@gmail.com>
2023-10-10 17:06:44 +0300

    add home link

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -276,7 +276,7 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
+                <Link href={"/"}>Home</Link>
               </li>
               <li>
                 <a>Parent</a>
@@ -315,7 +315,7 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <Link href={"/"}>Home</Link>
             </li>
             <li tabIndex={0}>
               <details>
