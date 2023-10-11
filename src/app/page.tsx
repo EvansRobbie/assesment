01e3214d5cfa6103173310961cdd279d58b0cbf4
EvansRobbie <evansrobbie5311@gmail.com>
2023-10-11 13:54:40 +0300

    add min height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default async function Home({
   return (
     <main className="flex min-h-screen flex-col ">
       <HeroSection cars={cars} />
-      <div className="  my-6 px-4 relative max-w-[1100px] mx-auto w-full lg:-right-1 xl:-right-20">
+      <div className="  my-6 px-4 relative max-w-[1100px] min-h-screen mx-auto w-full lg:-right-1 xl:-right-20">
         <ListCars cars={cars} isLoading={isLoading} popularMake={popularMake} />
       </div>
     </main>
